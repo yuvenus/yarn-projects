9ea6882105208dc2cc5cdf9377c0909a1feb1615
Venus Yu <venus0yu@gmail.com>
2020-02-21 18:10:07 -0500

    Updates

---
+++ precache-manifest.bf3d5d1ba187a91c215b6e2dd0fcc3e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3085de17f1a1cc579fc90d8c311a873",
+    "url": "/yarn-projects/index.html"
+  },
+  {
+    "revision": "17732131f3d4f24a7c06",
+    "url": "/yarn-projects/static/css/main.ad4528e0.chunk.css"
+  },
+  {
+    "revision": "8f2fd96fa30142cdb676",
+    "url": "/yarn-projects/static/js/2.361ff8ce.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/yarn-projects/static/js/2.361ff8ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17732131f3d4f24a7c06",
+    "url": "/yarn-projects/static/js/main.36218157.chunk.js"
+  },
+  {
+    "revision": "3604db376ad44c7826ce",
+    "url": "/yarn-projects/static/js/runtime-main.9ff714a9.js"
+  }
+]);
