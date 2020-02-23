d3d1baddd9df1f0453a8f2b518ef30f59a9c1bd5
Venus Yu <venus0yu@gmail.com>
2020-02-22 20:35:06 -0500

    adding header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Grid from "./Grid/Grid";
+import Header from "./Header/Header"
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Grid></Grid>
     </div>
   );
---
+++ src/Header/Header.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./Header.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { faSort } from '@fortawesome/free-solid-svg-icons';
+
+class Header extends Component {
+    state = {
+        expanded: false,
+        sort: false
+    }
+
+    changeExpanded = () => this.setState({expanded: !this.state.expanded});
+    // changeSort = (type) =>  
+
+    render() {
+        return <div className={this.state.expanded ? 'header expanded' : 'header'}>
+            <div className="buttons-ctnr">
+                <span className="button info" onClick={this.changeExpanded}>
+                    <FontAwesomeIcon className="icon" icon={faInfoCircle} />
+                </span>
+                {/* <span className="button sort" onClick={this.changeExpanded}>
+                    <FontAwesomeIcon className="icon" icon={faSort} />
+                </span> */}
+            </div>
+
+            {this.state.expanded && 
+                <p>This is my collection of stuffed animals I've crocheted/knitted (mostly the former) for friends 
+                    and coworkers so that they would have a friend to help them through stressful times. 
+                    The secondary goal was to shrink my yarn collection but I ended up buying more yarn instead! 😂</p>
+            }  
+        </div>
+    }
+}
+
+export default Header;
